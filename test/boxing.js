65b20b69efba6192b5a228b537265e8ebae6fde9
Thorsten Lorenz <thlorenz@gmx.de>
2014-06-14 12:20:51 -0400

    adding boxing tests

--- test/boxing.js
+++ test/boxing.js
@@ -2,6 +2,50 @@
 
 'use strict';
 
+/**
+ * ## 32-bit architecture
+ *
+  * 32-bit slot separation to hold a signed integer:
+  *
+  * - 1 bit to tag it as value
+  * - 1 bit for sign
+  * - 30 bits for actual value
+  *
+  * ### Producing 30 bits actual value
+  *
+  * #### In Detail (each left shift `<<` multiplies value by 2):
+  *
+  * ```
+  *   pad > ((1 << 30) - 1).toString(2)
+  *   '111111111111111111111111111111'
+  *
+  *   pad > ((1 << 30) - 1).toString(2).length
+  *   30
+  *
+  *   pad > ((1 << 30) - 1).toString(10)
+  *   '1073741823'
+  *
+  *   pad > (-(1 << 30)).toString(10)
+  *   '-1073741824'
+  * ```
+  *
+  * #### Short
+  *
+  * pad > console.log('min: %d, max: %d', -Math.pow(2, 30), Math.pow(2, 30) - 1)
+  * min: -1073741824, max: 1073741823
+  *
+  * ## 64-bit architecture
+  *
+  * - on x64 SMIs are 32-bit signed integers represented at higher half of 64-bit value
+  * - format: [32 bit signed int] [31 bits zero padding] 0
+  *
+  * #### Short
+  *
+  * pad > console.log('min: %d, max: %d', -Math.pow(2, 31), Math.pow(2, 31) - 1)
+  * min: -2147483648, max: 2147483647
+  *
+  */
+
 var test = require('tap').test;
 
 function isInteger(val) {
@@ -14,11 +58,11 @@ var is64Bit = /64/.test(process.arch);
 
 var min, max;
 if (is64Bit) {
-  max = 2147483647;
   min = -2147483648;
+  max = 2147483647;
 } else {
-  max = 1073741823;
   min = -1073741824;
+  max = 1073741823;
 }
 
 
